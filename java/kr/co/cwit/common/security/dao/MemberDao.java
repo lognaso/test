1127dc7d056ce058419b3d1478b0e2dcc040e43c
Le Van Long <lvlong1@cmcglobal.vn>
2022-10-03 13:57:26 +0700

    1

--- java/kr/co/cwit/common/security/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package kr.co.cwit.common.security.dao;
-
-import java.util.Map;
-
-public interface MemberDao {
-	public Map<String, Object> userLogin(String username);
-	
-	public int selectApprPopup(String username);
-}
